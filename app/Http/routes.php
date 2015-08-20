ff1698c2d626cbbed13b63010ec51dbba3cfa325
lferreira457 <lferreira457@github.com>
2015-08-20 15:49:13 +1000

    added RoomTypeController & updated routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,3 +14,10 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group(['prefix' => 'adminapi'], function()
+{
+    Route::resource('room_type', 'RoomTypeController');
+  
+});
+ 
